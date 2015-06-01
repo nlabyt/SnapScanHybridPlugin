bb390966ab0cafb4b08343d9b9ae01ae477e9792
nicolas <nlabyt@smallbusinessact.com>
2015-06-01 17:29:09 +0200

    next step
    Signed-off-by: nicolas <nlabyt@smallbusinessact.com>

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -17,6 +17,8 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import java.util.ArrayList;
 //PFU ScanSnap SDK classes
+//import com.fujitsu.pfu.mobile.device.ScanSnapSettings;
+
 import com.fujitsu.pfu.mobile.device.PFUDevice;
 import com.fujitsu.pfu.mobile.device.PFUDeviceError;
 import com.fujitsu.pfu.mobile.device.PFUDeviceManager;
@@ -26,8 +28,12 @@ import com.fujitsu.pfu.mobile.device.PFUSSDeviceManager;
 import com.fujitsu.pfu.mobile.device.SSDeviceScanSettings;
 import com.fujitsu.pfu.mobile.device.SSNotification;
 import com.fujitsu.pfu.mobile.device.SSDeviceError;
+import com.fujitsu.pfu.mobile.device.SSDevicePageInfo;
+
+
 
 import android.util.Log;
+import java.lang.reflect.Method;
 
 public class Hello extends CordovaPlugin {
     private static final String LOG_TAG = "SnapScanPlugin";
@@ -40,38 +46,41 @@ public class Hello extends CordovaPlugin {
 	private BroadcastReceiver broadcastReceiver = null;
 
 
+	//private static final int FILE_FORMAT = 0; (0 is jpeg 1 is pdf)
+	private static final int FILE_FORMAT = 1;
 	private static final String OUTPUT_PATH = "/mnt/sdcard/scansnap";
-	private static final String DEVICE_PASSWORD = "1234";
+	private static final String DEVICE_PASSWORD = "2155";
 	private PFUDeviceManager pfuDeviceManager;
 	private PFUSSDevice device = null;
-	
-	private Context getApplicationContext() {
-		return this.cordova.getActivity().getApplicationContext();
-	}
+	public String scan_action = "single";
+
 	private String convertIntToString(int i){
 		return Integer.toString(i);
 	}
+
 	@Override
 	public boolean execute(String action, String data, CallbackContext callbackContext) throws JSONException {
 		this.callbackContext = callbackContext;
-      	Log.v(LOG_TAG, "Init Test Logs");
+      	Log.v(LOG_TAG, "Init Test Logs datas : " + data);
 		setupBroadcastManager();
 
-		final String myTransportedDatas = data;
+		final String myTransportedDatas = data.replaceAll("\\W",""); // clean string ( very quick fix to del ["..."] )
+      	Log.v(LOG_TAG, "myTransportedDatas : " + myTransportedDatas);
 
 		if (action.equals("greet")) {
-            cordova.getThreadPool().execute(new Runnable() {
-            	String datas = myTransportedDatas;
+            cordova.getActivity().runOnUiThread(new Runnable() {
+            	String datas = new String(myTransportedDatas);
                 public void run() {
 					String message = "Hello, " + datas;
-					createCallback(message , true);
+
+					createCallback(message , true, true);
                 }
             });
             return true;
 		}
 		// test if the required action refers to the 'search' method
 		if (action.equals("search")){
-            cordova.getThreadPool().execute(new Runnable() {
+            cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
 					Search();
                 }
@@ -80,9 +89,10 @@ public class Hello extends CordovaPlugin {
 		}
 		// test if the required action refers to the 'scan' method
 		if (action.equals("scan")){
-            cordova.getThreadPool().execute(new Runnable() {
+            cordova.getActivity().runOnUiThread(new Runnable() {
+				String datas = myTransportedDatas;
                 public void run() {
-					Scan();
+					Scan(datas); // single or double page
 				}
             });
             return true;
@@ -90,23 +100,35 @@ public class Hello extends CordovaPlugin {
 		// no action matches, return error
 		return false;
 	}
-	private void createCallback(String info, boolean keepCallback){
-		PluginResult result = new PluginResult(PluginResult.Status.OK, info);
-            result.setKeepCallback(keepCallback);
-            this.callbackContext.sendPluginResult(result);
+	public void createCallback(String info, boolean keepCallback, boolean success){
+		PluginResult result = null;
+		if (success == false){
+			result = new PluginResult(PluginResult.Status.ERROR, info);			
+		} else {
+			result = new PluginResult(PluginResult.Status.OK, info);
+		}
+        result.setKeepCallback(keepCallback);
+        this.callbackContext.sendPluginResult(result);
     }
 	public void setupBroadcastManager(){
 		// if activity and received have not been created yet, create them
 		if(activity == null || broadcastReceiver == null){
-			app_context = this.getApplicationContext();
 			// Get the main activity
 			activity = this.cordova.getActivity();
+			// Get app context
+			app_context = this.cordova.getActivity().getApplicationContext();
 			// Use the local broadcast manager
 			localBroadcastManager = LocalBroadcastManager.getInstance(activity);
 			// Add actions
 			intentfilter.addAction(PFUNotification.ACTION_PFU_DEVICE_DID_CONNECT);
 			intentfilter.addAction(PFUNotification.ACTION_PFU_LIST_OF_DEVICES_DID_CHANGE);
 			intentfilter.addAction(SSNotification.ACTION_SS_DEVICE_DID_FINISH_SCAN);
+			// extra receiver for scan manager actions
+			intentfilter.addAction(SSNotification.ACTION_SS_DEVICE_DID_FINISH_MAKE_PDF);
+			intentfilter.addAction(SSNotification.EXTRA_DATA_SS_DEVICE_DID_FINISH_MAKE_PDF);
+			intentfilter.addAction(SSNotification.ACTION_SS_DEVICE_DID_SCAN_PAGE);
+			intentfilter.addAction(SSNotification.EXTRA_DATA_SS_DEVICE_DID_SCAN_PAGE);
+
 			// Create and connect a new broadcast receiver
 			broadcastReceiver = new MyBroadcastReceiver();
 			localBroadcastManager.registerReceiver(broadcastReceiver, intentfilter);
@@ -114,6 +136,7 @@ public class Hello extends CordovaPlugin {
 	}
 	private void Search()
 	{
+		device = null; // reset the device for sure remake all processus
 		// Create specific a device manager
 		pfuDeviceManager = PFUDeviceManager.getDeviceManagerWithType(PFUSSDeviceManager.class, PFUSSDeviceManager.PFUDEVICETYPE_SCANSNAP, app_context);
 		
@@ -121,9 +144,14 @@ public class Hello extends CordovaPlugin {
 		// Search for available devices
 		PFUDeviceError devError = pfuDeviceManager.searchForDevices(PFUDeviceManager.PFUSCANSNAP_ALL);
 		int errorCode = devError.getErrorCode();
-
+		if (errorCode != 0){
+			PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Device canot be searched error : "+convertIntToString(errorCode));
+			result.setKeepCallback(true);
+			callbackContext.sendPluginResult(result);
+		}
 		Log.v(LOG_TAG, "Search:: errorCode searchForDevices");
 		Log.v(LOG_TAG, this.convertIntToString(errorCode));
+		//this.createCallback("Search is begin " , true, true);
 
 	}
 	private void onPFUDeviceConnect()
@@ -142,41 +170,38 @@ public class Hello extends CordovaPlugin {
 			int errorCode = devErr.getErrorCode();
 			Log.v(LOG_TAG, "onPFUDeviceConnect:: errorCode conect");
 			Log.v(LOG_TAG, this.convertIntToString(errorCode));
-			// Create and setup scan settings
-			SSDeviceScanSettings m_scanSetting = new SSDeviceScanSettings();
-			m_scanSetting.setSaveFolderPath(OUTPUT_PATH); // Set image destination path
-			// Get the connected device
-			//device = (PFUSSDevice)pfuDeviceManager.getConnectedDevice();
-			// Start scan process
-			if(device != null){
-				devErr = device.beginScanSession();
-				errorCode = devErr.getErrorCode();
-				
-				Log.v(LOG_TAG, "Scan:: errorCode is beginScanSession");
-				Log.v(LOG_TAG, this.convertIntToString(errorCode));
-
-				devErr = device.scanDocuments(m_scanSetting);
-				errorCode = devErr.getErrorCode();
-
-				Log.v(LOG_TAG, "Scan:: errorCode is beginScanSession");
-				Log.v(LOG_TAG, this.convertIntToString(errorCode));
-
-				this.createCallback("Scan command sent." , true);
-				//callbackContext.success("Scan command sent.");
-			} else {
-				Log.v(LOG_TAG, "Scan:: device is null -> no scan, no callback");
+			
+			// my device is not connect error
+			if (errorCode != 0){
+				PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Device canot be conected error : "+convertIntToString(errorCode));
+    			result.setKeepCallback(true);
+    			callbackContext.sendPluginResult(result);
 			}
+			//String name = device.getDeviceName();
+			//Log.v(LOG_TAG, "Scan:: onPFUDeviceConnect:: "+name+" is connected ! ");
+			
+
+			//this.createCallback(name+" is connected ! " , true, true);
+
 		}
 	}
-	private void Scan()
+	public void Scan(String status)
 	{
+		Log.v (LOG_TAG, "Scan Entry Param : " + status);
+
 		// Create and setup scan settings
 		SSDeviceScanSettings m_scanSetting = new SSDeviceScanSettings();
+		m_scanSetting.setFileFormat(FILE_FORMAT);
 		m_scanSetting.setSaveFolderPath(OUTPUT_PATH); // Set image destination path
+
+		if (status.equals("double")){ // set recto/verso
+			Log.v (LOG_TAG, "DOUBLE PAGE active : " + status);
+			m_scanSetting.setMultiFeed(1);
+		}
 		// Get the connected device
 		device = (PFUSSDevice)pfuDeviceManager.getConnectedDevice();
 		// Start scan process
-		
+
 		if(device != null){
 			PFUDeviceError devErr = device.beginScanSession();
 			int errorCode = devErr.getErrorCode();
@@ -190,16 +215,34 @@ public class Hello extends CordovaPlugin {
 			Log.v(LOG_TAG, "Scan:: errorCode is beginScanSession");
 			Log.v(LOG_TAG, this.convertIntToString(errorCode));
 
-			this.createCallback("Scan command sent." , true);
+			// my device canot be conected.. callback them
+			if (errorCode != 0){
+				PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Error on scan function : "+ convertIntToString(errorCode));
+    			result.setKeepCallback(true);
+    			callbackContext.sendPluginResult(result);
+			}
+			// return the path file in callback
+			//this.createCallback(filePath , true);
+			//this.createCallback("Scan command sent." , true, true);
+			
+			// send control callback for test if command is proper sended
 			//callbackContext.success("Scan command sent.");
+		
+
 		} else {
 			Log.v(LOG_TAG, "Scan:: device is null -> no scan, no callback");
+			PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Error on scan function : device is null ");
+			result.setKeepCallback(true);
+			callbackContext.sendPluginResult(result);
 		}
 	}
 	//end class
 
 	// Handler for received Intents
 	class MyBroadcastReceiver extends BroadcastReceiver {
+		private Hello hello = new Hello();
+	    private static final String LOG_TAG = "SnapScanPlugin/MyBroadcastReceiver";
+
 		public void onReceive(Context context, Intent intent) {
 			// Add here any other event handlers
 			// ACTION_PFU_LIST_OF_DEVICES_DID_CHANGE event received
@@ -213,12 +256,73 @@ public class Hello extends CordovaPlugin {
 				device.disconnect();
 			}
 			if(intent.getAction().equals(PFUNotification.ACTION_PFU_DEVICE_DID_CONNECT))
-			{
+			{	
+				// CALBACK FOR SEARCH THE DEVICE
 				// A new devices has connected, popup a message
-				callbackContext.success("Connected to " +device.getDeviceName().toString());
+				//callbackContext.success("Connected to " +device.getDeviceName().toString());
+				
+				/*PluginResult result = new PluginResult(PluginResult.Status.OK, "Connected to " +device.getDeviceName().toString());
+    			result.setKeepCallback(true);
+    			callbackContext.sendPluginResult(result);*/
+
+    			// directly scan action ?
+    			Scan("single");
+
 			}
+			// callback for finish scan action 
+			// PDF
+			if(intent.getAction().equals(SSNotification.ACTION_SS_DEVICE_DID_FINISH_MAKE_PDF)) {
+			  	// Get the pdf path
+			  	String pdfPath = intent.getStringExtra(SSNotification.EXTRA_DATA_SS_DEVICE_DID_FINISH_MAKE_PDF);
+				Log.v(LOG_TAG, "Scan:: scan in pdf is finish, path of file :");
+				Log.v(LOG_TAG, pdfPath);
+			  	
+			  	// test the send the broadcast end scan
+				PluginResult result = new PluginResult(PluginResult.Status.OK, pdfPath);
+    			result.setKeepCallback(false);
+    			callbackContext.sendPluginResult(result);
+
+
+			}
+			// JPEG
+			if(intent.getAction().equals(SSNotification.ACTION_SS_DEVICE_DID_SCAN_PAGE)) {
+			  	SSDevicePageInfo information = (SSDevicePageInfo)intent.getSerializableExtra(SSNotification.EXTRA_DATA_SS_DEVICE_DID_SCAN_PAGE);
+
+			  	String frontJPEGPath = information.getFilePathFront();
+			  	// If duplex...
+			  	String backJPEGPath = information.getFilePathBack();
+
+				Log.v(LOG_TAG, "Scan:: scan in JPEG is finish, path of file 1 et 2 :");
+				Log.v(LOG_TAG, "Scan:: frontJPEGPath : "+frontJPEGPath);
+				Log.v(LOG_TAG, "Scan:: backJPEGPath : "+backJPEGPath);
+
+			  	JSONObject returnObject = new JSONObject();
+			  	try { // wanted.
+					returnObject.put("frontPage", frontJPEGPath);
+					returnObject.put("backPage", backJPEGPath);
+				} catch (JSONException e) {
+					Log.v(LOG_TAG, "SCAN : JSON EXCEPTION");
+				    // TODO Auto-generated catch block
+				    e.printStackTrace();
+				}
+
+				Log.v(LOG_TAG, "Scan:: scan in JPEG is finish, jsonobject for callback :");
+				Log.v(LOG_TAG, returnObject.toString());
+				
+				// return final object with filesPath
+				PluginResult result = new PluginResult(PluginResult.Status.OK, returnObject);
+    			result.setKeepCallback(false);
+    			callbackContext.sendPluginResult(result);
+
+				// return the two url of pages in a calback (json object)
+				//callbackContext.success("Connected to " +device.getDeviceName().toString());
+
+			  	//hello.createCallback(returnObject.toString() , true, true);
+
+			}
+
+
 		}
 	}
 	
 }
-
